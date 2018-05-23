7830ac23e3f0b329acfa0ac22bef3d8f1f0dd919
Janer Bastos De Melo <janer.melo@ufopa.edu.br>
2018-05-23 17:07:19 -0300

    Ajustes e Configuração

--- views.py
+++ views.py
@@ -1,3 +1,14 @@
+#-*- coding:UTF-8 -*-
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    template = 'gestor_espaco/index.html'
+
+    context = {
+        'page_name': 'Pagina inicial',
+        'detail_page_name': 'Detalhes da pagina inicial'
+    }
+
+    return render(request, template, context)
